b5994890086ad2c3e68e78f147653126f42a54b5
Place TwentySix <55628781+place26@users.noreply.github.com>
2024-02-09 09:44:33 +0800

    edit nvim-treesitter.lua

--- common/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,22 @@
-local config = function()
-	local configs = require("nvim-treesitter.configs")
-
-	configs.setup({
-		ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "python", "javascript", "html" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },  
-	  })
-end
-
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  run = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      -- 여기에 설치하고자 하는 언어를 추가합니다.
+      ensure_installed = { "lua", "vim", "vimdoc", "markdown", "python" },
+
+      -- 자동으로 구문 강조를 활성화합니다.
+      highlight = {
+        enable = true, -- `false`로 설정하면 구문 강조가 비활성화됩니다.
+        additional_vim_regex_highlighting = false,
+      },
+      
+      -- 여기에 다른 nvim-treesitter 모듈 설정을 추가할 수 있습니다.
+      -- 예를 들어, 코드 접기(folding)나 인덴테이션 조정 등의 기능을 활성화할 수 있습니다.
+      indent = {
+        enable = true, -- 언어별 자동 인덴테이션 활성화
+      },
+    }
+  end
 }
