b81cdfc5365d8cedba0cbea671c8fa02a137e988
Place TwentySix <55628781+place26@users.noreply.github.com>
2024-02-09 09:19:02 +0800

    add vim-fugitive.lua

---
+++ common/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -0,0 +1,9 @@
+return {
+  'tpope/vim-fugitive',
+  config = function()
+    -- 여기에 `fugitive.vim`에 대한 추가적인 Neovim 설정을 추가할 수 있습니다.
+    -- 예를 들어, 특정 키 바인딩을 설정하거나, `fugitive.vim`의 기능을 확장하는 코드를 여기에 포함시킬 수 있습니다.
+    -- 기본적으로 `fugitive.vim`은 추가 설정 없이도 잘 작동합니다.
+  end
+}
+
