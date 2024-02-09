d51eb406b1270d3edd6315054b61d54ac0d61bbb
Place TwentySix <55628781+place26@users.noreply.github.com>
2024-02-09 09:20:23 +0800

    add nvim-treesitter.lua

--- common/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,38 +1,15 @@
 local config = function()
-	require("nvim-treesitter.configs").setup({
-		indent = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
-		ensure_installed = {
-			"markdown",
-			"json",
-			"javascript",
-			"typescript",
-			"yaml",
-			"html",
-			"css",
-			"markdown",
-			"bash",
-			"lua",
-			"dockerfile",
-			"solidity",
-			"gitignore",
-			"python",
-			"vue",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-	})
+	local configs = require("nvim-treesitter.configs")
+
+	configs.setup({
+		ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "python", "javascript", "html" },
+		sync_install = false,
+		highlight = { enable = true },
+		indent = { enable = true },  
+	  })
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = config
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
 }
