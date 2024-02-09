23b7d16ecf397db086758b5aed3f71f5076b6f68
Place TwentySix <55628781+place26@users.noreply.github.com>
2024-02-09 09:19:46 +0800

    add comment.lua

--- common/.config/nvim/lua/plugins/comment.lua
+++ common/.config/nvim/lua/plugins/comment.lua
@@ -1,4 +1,10 @@
 return {
   "numToStr/Comment.nvim",
-  lazy = false,
+  config = function()
+    require('Comment').setup {
+      -- 여기에 필요한 설정 옵션을 추가합니다.
+      -- 예를 들어, 주석 스타일을 사용자 정의하거나, 키 바인딩을 설정하는 등의 작업을 수행할 수 있습니다.
+      -- 기본 설정으로도 충분히 유용하므로, 특별한 필요가 없다면 이 부분을 비워 두어도 됩니다.
+    }
+  end
 }
